25478120b31330bb8a0604d611815d29aa933bb3
ceLLro <cellro18@gmail.com>
2020-07-05 12:21:29 +0300

    Final commit.

--- js/end.js
+++ js/end.js
@@ -2,14 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-
 finalScore.innerText = mostRecentScore;
-
 
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
